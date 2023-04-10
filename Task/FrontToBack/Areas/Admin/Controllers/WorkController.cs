7c1143beb48aa89846e454b2293ed19c8ec8add4
Alakbarbh <alakbarbh@code.edu.az>
2023-04-10 12:39:18 -0800

    Area, Admin panel

---
+++ Task/FrontToBack/Areas/Admin/Controllers/WorkController.cs
@@ -0,0 +1,23 @@
+﻿using FrontToBack.Data;
+using FrontToBack.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FrontToBack.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WorkController : Controller
+    {
+
+        private readonly AppDbContext _context;
+        public WorkController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Recent> recents = await _context.Recents.Where(m => !m.SoftDelete).ToListAsync();
+            return View(recents);
+        }
+    }
+}
